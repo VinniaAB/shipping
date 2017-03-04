248078c5649f5c75c4b007f756bbe5bb4b098436
Johan Björk <johanimon@gmail.com>
2017-03-04 23:16:09 +0100

    Add CompositeService

---
+++ src/CompositeService.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: johan
+ * Date: 2017-03-04
+ * Time: 17:08
+ */
+
+namespace Vinnia\Shipping;
+
+
+use GuzzleHttp\Promise\PromiseInterface;
+
+class CompositeService implements ServiceInterface
+{
+
+    /**
+     * @var ServiceInterface[]
+     */
+    private $delegates;
+
+    /**
+     * CompositeService constructor.
+     * @param ServiceInterface[] $delegates
+     */
+    function __construct(array $delegates)
+    {
+        $this->delegates = $delegates;
+    }
+
+    /**
+     * @param Address $sender
+     * @param Address $recipient
+     * @param Package $package
+     * @return PromiseInterface promise resolved with an array of \Vinnia\Shipping\Quote on success
+     */
+    public function getQuotes(Address $sender, Address $recipient, Package $package): PromiseInterface
+    {
+        return $this->aggregate('getQuotes', [$sender, $recipient, $package]);
+    }
+
+    /**
+     * @param string $trackingNumber
+     * @return PromiseInterface
+     */
+    public function getTrackingStatus(string $trackingNumber): PromiseInterface
+    {
+        return $this->aggregate('getTrackingStatus', [$trackingNumber]);
+    }
+
+    private function aggregate(string $method, array $parameters = []): PromiseInterface
+    {
+        /* @var PromiseInterface[] $promises */
+        $promises = array_map(function (ServiceInterface $service) use ($method, $parameters): PromiseInterface {
+            return call_user_func_array([$service, $method], $parameters);
+        }, $this->delegates);
+
+        // create an aggregate promise that will be fulfilled
+        // when all service promises are either fulfilled or rejected
+        $aggregate = \GuzzleHttp\Promise\settle($promises);
+
+        return $aggregate->then(function (array $inspections) {
+            $quotes = [];
+            foreach ($inspections as $inspection) {
+                if ($inspection['state'] === PromiseInterface::FULFILLED) {
+                    $quotes[] = $inspection['value'];
+                }
+            }
+            return $quotes;
+        });
+    }
+}
